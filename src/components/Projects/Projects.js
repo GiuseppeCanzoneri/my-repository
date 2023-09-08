e83b352295e30126b0a660b68779ffa5c16ad897
Giuseppe Canzoneri <gcanzoneri6@gmail.com>
2023-09-08 18:52:09 +0200

    posizione progetti

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -15,7 +15,7 @@ function Projects() {
         </h1>
         <p style={{ color: "white" }}>Here are a few projects I've worked on recently.</p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={8} className="project-card">
+          <Col md={4} className="project-card">
             <ProjectCard
               imgPath={travelPlanner}
               isBlog={false}
@@ -23,11 +23,13 @@ function Projects() {
               description="Travel Planner è un'applicazione completa per la pianificazione e la gestione dei viaggi. Sfrutta le potenzialità di React per il frontend, Spring Boot per il backend e Redux per la gestione dello stato. Con Travel Planner, gli utenti possono facilmente organizzare i loro viaggi, ottenere itinerari personalizzati e gestire le proprie informazioni utente, il tutto in un'unica piattaforma intuitiva. 🗺️📅"
               ghLink="https://github.com/GiuseppeCanzoneri/CAPSTONE-FRONTEND-Travel-Planner/tree/master"
             />
+          </Col>
+          <Col md={4} className="project-card">
             <ProjectCard
               imgPath={Meteo}
               isBlog={false}
               title="Sky Cast Live 🌤️"
-              description=" Scoprite il potere di conoscere in anticipo le condizioni meteorologiche con Sky Cast Live! ☔ Grazie all'utilizzo di React e Redux, ho dato vita a un'app intuitiva e coinvolgente per tenervi informati su sole, pioggia o neve. Prendete il controllo della vostra giornata con previsioni in tempo reale e avvisi personalizzati. Non lasciate che il tempo vi sorprenda, lasciate che Sky Cast Live vi dia una mano! 🌦️📊"
+              description="Scoprite il potere di conoscere in anticipo le condizioni meteorologiche con Sky Cast Live! ☔ Grazie all'utilizzo di React e Redux, ho dato vita a un'app intuitiva e coinvolgente per tenervi informati su sole, pioggia o neve. Prendete il controllo della vostra giornata con previsioni in tempo reale e avvisi personalizzati. Non lasciate che il tempo vi sorprenda, lasciate che Sky Cast Live vi dia una mano! 🌦️📊"
               ghLink="https://github.com/GiuseppeCanzoneri/meteo-react"
             />
           </Col>
