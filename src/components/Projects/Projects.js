418c4876bdc58cbc18a18822e5c98c5c126fb821
Giuseppe Canzoneri <gcanzoneri6@gmail.com>
2023-09-08 18:21:40 +0200

    add project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import travelPlanner from "../../Assets/Projects/planner.png";
+import skyCast from "../..//Assets/Projects/skyCast.png";
 
 function Projects() {
   return (
@@ -21,6 +22,13 @@ function Projects() {
               title="Travel Planner 🛩️"
               description="Travel Planner è un'applicazione completa per la pianificazione e la gestione dei viaggi. Sfrutta le potenzialità di React per il frontend, Spring Boot per il backend e Redux per la gestione dello stato. Con Travel Planner, gli utenti possono facilmente organizzare i loro viaggi, ottenere itinerari personalizzati e gestire le proprie informazioni utente, il tutto in un'unica piattaforma intuitiva. 🗺️📅"
               ghLink="https://github.com/GiuseppeCanzoneri/CAPSTONE-FRONTEND-Travel-Planner/tree/master"
+            />
+            <ProjectCard
+              imgPath={skyCast}
+              isBlog={false}
+              title="Sky Cast Live"
+              description="🌤️ Scoprite il potere di conoscere in anticipo le condizioni meteorologiche con Sky Cast Live! ☔ Grazie all'utilizzo di React e Redux, ho dato vita a un'app intuitiva e coinvolgente per tenervi informati su sole, pioggia o neve. Prendete il controllo della vostra giornata con previsioni in tempo reale e avvisi personalizzati. Non lasciate che il tempo vi sorprenda, lasciate che Sky Cast Live vi dia una mano! 🌦️📊"
+              ghLink="https://github.com/GiuseppeCanzoneri/meteo-react"
             />
           </Col>
         </Row>
