404b2f5ef4f5ba0ac1304f99a313bfe8d52fb3b0
Giuseppe Canzoneri <gcanzoneri6@gmail.com>
2023-09-09 14:45:55 +0200

    Aggiunto Spotify clone

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,6 +4,7 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import travelPlanner from "../../Assets/Projects/planner.png";
 import Meteo from "../..//Assets/Projects/Meteo.png";
+import Spotify from "../../Assets/Projects/spotify.png";
 
 function Projects() {
   return (
@@ -31,6 +32,15 @@ function Projects() {
               title="Sky Cast Live 🌤️"
               description="Scoprite il potere di conoscere in anticipo le condizioni meteorologiche con Sky Cast Live! ☔ Grazie all'utilizzo di React e Redux, ho dato vita a un'app intuitiva e coinvolgente per tenervi informati su sole, pioggia o neve. Prendete il controllo della vostra giornata con previsioni in tempo reale e avvisi personalizzati. Non lasciate che il tempo vi sorprenda, lasciate che Sky Cast Live vi dia una mano! 🌦️📊"
               ghLink="https://github.com/GiuseppeCanzoneri/meteo-react"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={Spotify}
+              isBlog={false}
+              title="Spostify Clone"
+              description="Un clone di Spotify realizzato con React è un'applicazione web che riproduce le principali funzionalità di Spotify, consentendo agli utenti di cercare, riprodurre e gestire la musica. L'applicazione presenta un'interfaccia utente simile a quella di Spotify, con schede per la home, la ricerca, la libreria e le playlist personali. Gli utenti possono effettuare l'accesso, cercare brani, ascoltare musica in streaming, creare playlist e gestire il proprio account. La tecnologia principale utilizzata è React, con eventuali framework di stile come Bootstrap per la progettazione."
+              ghLink="https://github.com/GiuseppeCanzoneri/spotify-redux"
             />
           </Col>
         </Row>
